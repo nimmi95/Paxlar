cf87ecf356a7fc422d9e9ea721eb420a00420504
nimmi95 <nabidasultana@gmail.com>
2020-06-30 01:17:38 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+  });
+
+  
